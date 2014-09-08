83cbdaa5fcd1b38d855ba5919b02d6c2478bde39
lancerabbit@gmail.com <lancerabbit@gmail.com@ebc87b04-d3cf-fd99-6881-f3b49f14b321>
2014-09-08 06:50:46 +0000

    Fix bug.

--- src/model/SpotImgDAO.java
+++ src/model/SpotImgDAO.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.util.Set;
+
 public interface SpotImgDAO {
 	public abstract SpotImg insert(SpotImg img);
 	public abstract int delete(SpotImg img);
 	public abstract int deleteBySpotId(String spotId);
 	public abstract SpotImg update(SpotImg img);
 	public abstract SpotImg selectByImgId(String id);
-	public abstract SpotImg selectBySpotId(String id);
+	public abstract Set<SpotImg> selectBySpotId(String id);
 }
