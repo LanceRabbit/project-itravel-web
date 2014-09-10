069498779a8024f5d38bd21fcf6a9ff9f18abd8e
lancerabbit@gmail.com <lancerabbit@gmail.com@ebc87b04-d3cf-fd99-6881-f3b49f14b321>
2014-09-10 14:31:08 +0000

    1. Delete the test class for connecting database by DriverManager. 2. Support pagination by query : retrieve at most 9 spots each page.

--- src/model/SpotDetailDAO.java
+++ src/model/SpotDetailDAO.java
@@ -1,9 +1,13 @@
 package model;
 
+import java.util.List;
+
 
 public interface SpotDetailDAO {
 	public abstract SpotDetail insert(SpotDetail spot);
 	public abstract int delete(SpotDetail spot);
 	public abstract SpotDetail update(SpotDetail spot);
 	public abstract SpotDetail select(String id);
+	
+	public abstract List<SpotDetail> select(int pageNo);
 }
