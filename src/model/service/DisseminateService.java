e88ab02efd28f50103550f988d68924e3d9dc2a3
lancerabbit@gmail.com <lancerabbit@gmail.com@ebc87b04-d3cf-fd99-6881-f3b49f14b321>
2014-10-03 17:44:22 +0000

    by Angela

--- src/model/service/DisseminateService.java
+++ src/model/service/DisseminateService.java
@@ -1,7 +1,5 @@
 package model.service;
 
-
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -25,77 +23,95 @@ public class DisseminateService {
 	Ad ad = new Ad();
 	CouponsDAO daoCoupon = new CouponsDAOHibernate();
 	Coupons coupon = new Coupons();
-	//從account 去找他自己的景點
-	//取得景點ID
-	
-	public static void main(String[] args) {
-		
 
+	// 從account 去找他自己的景點
+	// 取得景點ID
+
+	public static void main(String[] args) {
+
+		DisseminateService s = new DisseminateService();
+		s.findAdsByAccountId("M14100001");
+		
 	}
 
-	public Set<SpotDetail> findSpotId(String AccountId){
-		
-		
+	public Set<SpotDetail> findSpotId(String AccountId) {
+
 		AccountDAO daoAcc = new AccountDAOHibernate();
 		Account acc = new Account();
 		acc = daoAcc.selectById(AccountId);
-		Set<SpotDetail> result = acc.getSpotDetails();	
-	
+		Set<SpotDetail> result = acc.getSpotDetails();
+		Iterator<SpotDetail> detail = acc.getSpotDetails().iterator();
+		while (detail.hasNext()) {
+
+			SpotDetail spot = detail.next();
+			Iterator<Ad> ad = spot.getAds().iterator();
+
+		}
+
 		return result;
-		
+
 	}
-	
-	public List<Ad> findAds(String spotId){
-		
+
+	public List<Ad> findAds(String spotId) {
+
 		List<Ad> result = dao.selectBySpotId(spotId);
+
+		return result;
+	}
+
+	public List<Ad> findAdsByAccountId(String accountId) {
+
+		List<Ad> result = dao.selectByAccountId(accountId);	
+		System.out.println("Ad list:"+result);
+		
+		
 		
 		return result;
 	}
-	public java.util.Date findDate(){
-		
+
+	public java.util.Date findDate() {
+
 		java.util.Date date = new java.util.Date();
 		date = dao.addDate(date, 7);
 		return date;
 	}
-	
-	public boolean AdInsert(String spotId ,byte[] image){
-		
+
+	public boolean AdInsert(String spotId, byte[] image) {
+
 		SpotDetail spot = new SpotDetail();
 		spot.setSpotId(spotId);
 		ad.setSpotDetail(spot);
 		java.util.Date date = new java.util.Date();
-		date = dao.addDate(date, 7);	
+		date = dao.addDate(date, 7);
 		ad.setTempAdId("EMP");
 		ad.setValidDay(date);
 		ad.setAdImg(image);
-		
-		if(dao.insert(ad)!=null){
+
+		if (dao.insert(ad) != null) {
 			return true;
-		}else{
+		} else {
 			return false;
-		}		
-		
+		}
+
 	}
-	
-	public boolean CouponInsert(String spotId,String text,java.util.Date date ,byte[] image) {
+
+	public boolean CouponInsert(String spotId, String text,
+			java.util.Date date, byte[] image) {
 		SpotDetail spot = new SpotDetail();
 		spot.setSpotId(spotId);
 		coupon.setSpotDetail(spot);
 		coupon.setCouponDescription(text);
-		coupon.setCouponImg(image);		
+		coupon.setCouponImg(image);
 		coupon.setValidDay(date);
 		coupon.setTempCouponId("EMP");
-		coupon =daoCoupon.insert(coupon);
-		if(coupon!=null){
+		coupon = daoCoupon.insert(coupon);
+		if (coupon != null) {
 			System.out.println(coupon.toString());
 			return true;
-		}else{
+		} else {
 			return false;
 		}
-		
-		
-		
+
 	}
-	
-	
+
 }
