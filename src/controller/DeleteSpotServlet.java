5b135a3022bd878e33123894de27f7b77ec82201
lancerabbit@gmail.com <lancerabbit@gmail.com@ebc87b04-d3cf-fd99-6881-f3b49f14b321>
2014-10-05 03:34:43 +0000

    Support spot deletion.

--- src/controller/DeleteSpotServlet.java
+++ src/controller/DeleteSpotServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.omg.CORBA_2_3.portable.OutputStream;
+
 import model.service.DeleteSpotService;
 
 @WebServlet("/control/DeleteSpot")
@@ -16,25 +19,33 @@ public class DeleteSpotServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	private void process(HttpServletRequest request, HttpServletResponse response){
-		System.out.println("delete spot servlet.....");
+		//System.out.println("delete spot servlet.....");
+		PrintWriter out = null;
 		
 		try {
 			request.setCharacterEncoding("UTF-8");
 			response.setCharacterEncoding("UTF-8");
 			response.setContentType("text/html");
+			out = response.getWriter();
 			
 			String spotId = request.getParameter("SpotId");
 			System.out.println("spotId : " + spotId);
 
+			
 			DeleteSpotService service = new DeleteSpotService();
-			if(service.deleteSpotById(spotId) == 0) {
-				System.out.println("redirect to : " + request.getContextPath()+"/spot/MySpot.jsp");
-				response.sendRedirect(request.getContextPath()+"/spot/MySpot.jsp");
-			}
+			if(service.deleteSpotById(spotId) == 0) 
+				out.print("true");
+			else 
+				out.print("false");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if(out != null) {
+				out.flush();
+				out.close();
+			}
 		}
 	}
 	
